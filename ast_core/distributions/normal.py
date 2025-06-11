4c6c751f4e843a83283070d081e5e2778ff977fa
Andreas Raja Goklas Sitorus <andreasrajagoklassitorus@gmail.com>
2025-06-11 15:54:25 +0200

    feat: Update policies, and necessary utils files

---
+++ ast_core/distributions/normal.py
@@ -0,0 +1,8 @@
+"""
+Multivariate normal distribution with mean and standard deviation outputted by a neural network
+"""
+
+import tensorflow as tf
+import numpy as np
+
+from ast_core.nn_models.mlp import mlp
